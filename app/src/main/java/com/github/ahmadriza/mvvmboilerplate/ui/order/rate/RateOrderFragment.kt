66a5cccb444f58fb7c3116a3ed9a808ce05e0473
Ahmad Riza <riza.public@gmail.com>
2021-02-06 16:05:14 +0700

    layout fragment rating order

--- app/src/main/java/com/github/ahmadriza/mvvmboilerplate/ui/order/rate/RateOrderFragment.kt
+++ app/src/main/java/com/github/ahmadriza/mvvmboilerplate/ui/order/rate/RateOrderFragment.kt
@@ -1,12 +1,12 @@
-package com.github.ahmadriza.mvvmboilerplate.ui.order
+package com.github.ahmadriza.mvvmboilerplate.ui.order.rate
 
 import com.github.ahmadriza.mvvmboilerplate.R
-import com.github.ahmadriza.mvvmboilerplate.databinding.FragmentOrderBinding
+import com.github.ahmadriza.mvvmboilerplate.databinding.FragmentOrderRatingBinding
 import com.github.ahmadriza.mvvmboilerplate.utils.base.BaseFragment
 
-class CreateOrderFragment : BaseFragment<FragmentOrderBinding>() {
+class RateOrderFragment : BaseFragment<FragmentOrderRatingBinding>() {
 
-    override fun getLayoutResource(): Int = R.layout.fragment_order
+    override fun getLayoutResource(): Int = R.layout.fragment_order_rating
 
     override fun initViews() {
         TODO("Not yet implemented")
