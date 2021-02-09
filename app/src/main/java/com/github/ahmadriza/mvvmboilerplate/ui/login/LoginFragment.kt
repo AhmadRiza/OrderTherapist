fc88e06fb2e7f6c25c7e9b4b2e918dd1f5a77070
Ahmad Riza <riza.public@gmail.com>
2021-02-10 01:03:38 +0700

    layout login

---
+++ app/src/main/java/com/github/ahmadriza/mvvmboilerplate/ui/login/LoginFragment.kt
@@ -0,0 +1,24 @@
+package com.github.ahmadriza.mvvmboilerplate.ui.login
+
+import com.github.ahmadriza.mvvmboilerplate.R
+import com.github.ahmadriza.mvvmboilerplate.databinding.FragmentLoginBinding
+import com.github.ahmadriza.mvvmboilerplate.utils.base.BaseFragment
+
+class LoginFragment : BaseFragment<FragmentLoginBinding>() {
+
+    override fun getLayoutResource(): Int = R.layout.fragment_login
+
+    override fun initViews() {
+
+    }
+
+    override fun initObservers() {
+
+    }
+
+    override fun initData() {
+
+    }
+
+
+}
