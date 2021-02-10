ce776c65c2f943a77d1430d6a1e7f74b102ab28e
Ahmad Riza <riza.public@gmail.com>
2021-02-10 20:49:23 +0700

    layout register

--- app/src/main/java/com/github/ahmadriza/mvvmboilerplate/ui/login/LoginFragment.kt
+++ app/src/main/java/com/github/ahmadriza/mvvmboilerplate/ui/login/LoginFragment.kt
@@ -1,5 +1,6 @@
 package com.github.ahmadriza.mvvmboilerplate.ui.login
 
+import androidx.navigation.fragment.findNavController
 import com.github.ahmadriza.mvvmboilerplate.R
 import com.github.ahmadriza.mvvmboilerplate.databinding.FragmentLoginBinding
 import com.github.ahmadriza.mvvmboilerplate.utils.base.BaseFragment
@@ -9,6 +10,10 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>() {
     override fun getLayoutResource(): Int = R.layout.fragment_login
 
     override fun initViews() {
+
+        binding.btnSignUp.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
 
     }
 
